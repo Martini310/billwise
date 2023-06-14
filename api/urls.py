ade36ab16ce5344233b0c6508096024b324a3524
Martini310 <martin310@wp.pl>
2023-06-14 22:26:51 +0200

    feat(api): Add new views and endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import InvoiceList
+from .views import InvoiceList, InvoiceCreate, MediaList, AccountCreate
 
 urlpatterns = [
-    path('invoices/', InvoiceList.as_view(), name='invoice_list'),
+    path('invoices/', InvoiceList.as_view()),
+    path('invoices/add/', InvoiceCreate.as_view()),
+    path('media/', MediaList.as_view()),
+    path('account/', AccountCreate.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from base.models import Invoice
-from .serializer import InvoiceSerializer
+from rest_framework import status
+from base.models import Invoice, Media, Supplier, Account
+from .serializer import InvoiceSerializer, MediaSerializer, SupplierSerializer, AccountSerializer
 
 
 class InvoiceList(APIView):
@@ -9,3 +10,75 @@ class InvoiceList(APIView):
         invoices = Invoice.objects.all()
         serializer = InvoiceSerializer(invoices, many=True)
         return Response(serializer.data)
+
+
+class InvoiceCreate(APIView):
+    def post(self, request):
+        serializer = InvoiceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MediaList(APIView):
+    def get(self, request):
+        media = Media.objects.all()
+        serializer = MediaSerializer(media, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MediaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SupplierList(APIView):
+    def get(self, request):
+        supplier = Supplier.objects.all()
+        serializer = SupplierSerializer(supplier, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SupplierSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Supplier(APIView):
+    def get_supplier_by_pk(self, pk):
+        try:
+            supplier = Supplier.objects.get(pk=pk)
+        except:
+            return Response({
+                'error':  'Supplier does not exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        supplier = self.get_supplier_by_pk(pk)
+        serializer = SupplierSerializer(supplier)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        supplier = self.get_supplier_by_pk(pk)
+        serializer = SupplierSerializer(supplier, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AccountCreate(APIView):
+    def post(self, request):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
