c2dc134e8f7cffa760b27dda49aa750826520f1e
Martini310 <martin310@wp.pl>
2023-06-18 20:56:18 +0200

    feat(api): Add views and endpoints for login/logout

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import InvoiceList, InvoiceCreate, InvoiceDetails, MediaList, MediaDetails, AccountCreate, SupplierList, \
-    SupplierDetails, RegisterAPI
+    SupplierDetails, RegisterAPI, LoginAPI
+from knox import views as knox_views
+
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('invoices/', InvoiceList.as_view()),
     path('invoices/<int:pk>', InvoiceDetails.as_view()),
     path('invoices/add/', InvoiceCreate.as_view()),
--- api/views.py
+++ api/views.py
@@ -5,6 +5,9 @@ from base.models import Invoice, Media, Supplier, Account
 from .serializers import InvoiceSerializer, MediaSerializer, SupplierSerializer, AccountSerializer, \
     UserSerializer, RegisterSerializer
 from knox.models import AuthToken
+from django.contrib.auth import login
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
 
 
 # Register API
@@ -19,6 +22,17 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
 
 
 class InvoiceList(APIView):
