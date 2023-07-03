a3bb835ef6cef6bfb33f72a0570788224dd0a2b9
Martini310 <martin310@wp.pl>
2023-07-03 22:35:21 +0200

    feat(front): Init React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/Button';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <p>siemka</p>
+      <Button />
+    </>
+  );
+}
+
