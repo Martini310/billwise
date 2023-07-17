d1b511cc2f0f3162f0ba9d9cc07b2d6eadc1620d
Martini310 <martin310@wp.pl>
2023-07-17 23:26:03 +0200

    Struggling with dropdown menu

--- billwise/settings.py
+++ billwise/settings.py
@@ -152,7 +152,7 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = "users.NewUser"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
--- frontend/src/components/addAccount.jsx
+++ frontend/src/components/addAccount.jsx
@@ -17,8 +17,41 @@ export function AddAccount() {
 		axiosInstance.get(suppliers_link).then((res) => {
       const data = res.data;
 			setSuppliers( data );
+      data.map((sup) => {
+        console.log(sup)
+        console.log(sup.name)
+        console.log(sup.id)
+      });
 		});
 	}, []);
+
+
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  
+  const [password, setPassword] = useState('');
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  
+
+  const [selectedSupplier, setSelectedSupplier] = useState('Enea');
+
+  // const handleSupplierChange = (event) => {
+  //   setSelectedSupplier(event.target.value);
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email);
+    console.log(password);
+    console.log(selectedSupplier);
+  };
 
   return (
     <Box
@@ -32,16 +65,22 @@ export function AddAccount() {
       }}
     >
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
+      <form onSubmit={handleSubmit}>
     
         {suppliers.length > 0 && (
-          <Select color="info" placeholder="Wybierz dostawcę" size="lg" variant="outlined">
-            {suppliers.map((supplier) => (
-              <Option key={supplier.id} value={supplier.id}>
+          <Select 
+            value={selectedSupplier}
+            onChange={e => {
+              console.log(e.target.attributes);
+              setSelectedSupplier(e)}}
+            
+            color="info" 
+            placeholder="Wybierz dostawcę" 
+            size="lg" 
+            variant="outlined"
+            >
+            {suppliers.map((supplier, index) => (
+              <Option key={index} value={supplier.name}>
                 {supplier.name}
               </Option>
             ))};
@@ -49,11 +88,19 @@ export function AddAccount() {
         )}
 
         <Input
+          type="email" 
+          id="email" 
+          value={email}
+          onChange={handleEmailChange}
           placeholder="Adres e-mail"
           required
           sx={{ mb: 1, fontSize: 'var(--joy-fontSize-sm)' }}
         />
         <Input
+          name='password'
+          id="password" 
+          value={password}
+          onChange={handlePasswordChange}
           placeholder="Hasło"
           required
           sx={{ mb: 1, fontSize: 'var(--joy-fontSize-sm)' }}
