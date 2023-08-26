feeb5d7d0333dc134bf3693670a5344d32102552
Martini310 <martin310@wp.pl>
2023-08-26 21:42:10 +0200

    Install Celery and add first simple demo task

---
+++ billwise/celery.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'billwise.settings')
+
+app = Celery('billwise')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+    
