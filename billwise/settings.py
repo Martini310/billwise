6deeefb7aaded439a6c4fc1b8ce169d42149fc08
Martini310 <martin310@wp.pl>
2023-09-12 23:04:33 +0200

    fix cors

--- billwise/settings.py
+++ billwise/settings.py
@@ -177,7 +177,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://billwise.onrender.com",
-    "https://billwise.onrender.com/"
 ]
 
 # Custon user model
