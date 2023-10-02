31f12f6fe9be9b5c09df3b23b83d0977d21fd440
Martini310 <martin310@wp.pl>
2023-10-02 22:39:07 +0200

    feat(docker): Configure docker files to work properly

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // except for webpack, other parts are left as generated
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
