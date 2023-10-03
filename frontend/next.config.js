2214370dc65a7527a74dc5e6845cea1dcae7f705
Martini310 <martin310@wp.pl>
2023-10-03 21:33:27 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  // except for webpack, other parts are left as generated
-  webpack: (config, context) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-    return config
-  }
-}
-
-module.exports = nextConfig
+  // exclude: [/node_modules/, /build/, /dist/, /last/],
+};
