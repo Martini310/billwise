f8e7d1a16d3a6ea8fc03213fb951f864d3f4ce3c
Martini310 <martin310@wp.pl>
2023-10-23 16:10:27 +0200

    Add new link to CORS_ALLOWED_ORIGINS

--- billwise/settings.py
+++ billwise/settings.py
@@ -189,10 +189,13 @@ REST_FRAMEWORK = {
     ],
 }
 
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://billwise.onrender.com",
+    "https://bill-wise.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
