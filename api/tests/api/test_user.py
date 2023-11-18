45bd73a0541a61c40f5e4a5b27a153993386cbdd
Martini310 <martin310@wp.pl>
2023-11-18 22:19:31 +0100

    test(api): Create test tests

---
+++ api/tests/api/test_user.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.django_db
+def test_register_user():
+    pass
---
+++ api/tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+import json
+from rest_framework import status
+from rest_framework.test import APIClient
+# from users.models import NewUser
+# from base.models import Category, Account, Invoice, Supplier
+
+
+client = APIClient()
+
+def test_pytest_working():
+    assert True == True
