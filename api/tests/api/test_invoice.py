e4cf9be01cbc6f37b53c2dd9abc89a9208720694
Martini310 <martin310@wp.pl>
2023-11-26 21:55:34 +0100

    test(api): Add create invoice test

--- api/tests/api/test_invoice.py
+++ api/tests/api/test_invoice.py
@@ -1,6 +1,7 @@
 import pytest
 from datetime import datetime
 from rest_framework import status
+from users.models import NewUser
 
 
 @pytest.mark.django_db
@@ -37,4 +38,27 @@ def test_not_user_get_invoice_fail(client, invoice):
 def test_user_get_another_user_invoice_fail(auth_client2, invoice):
     response = auth_client2.get('/api/invoices/1/', follow=True)
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN # NOT OK
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.data['detail'] == "You are not allowed to see this content"
+
+@pytest.mark.django_db
+def test_user_creates_new_invoice(auth_client, category):
+    payload = {
+        'number': 'test/123',
+        'date': datetime.today().date().strftime("%Y-%m-%d"),
+        'amount': 123,
+        'pay_deadline': datetime.today().date().strftime("%Y-%m-%d"),
+        'start_date': datetime.today().date().strftime("%Y-%m-%d"),
+        'end_date': datetime.today().date().strftime("%Y-%m-%d"),
+        'amount_to_pay': 123,
+        'wear': 100,
+        'user': 1,
+        'is_paid': False,
+        'consumption_point': 'Test point',
+        'category': 1,
+        'bank_account_number': '12 1234 5678 9012 3456 7890 1234',
+        'transfer_title': 'test title',
+    }
+    response = auth_client.post('/api/invoices/', payload)
+    print(response.data)
+    assert response.status_code == status.HTTP_201_CREATED
