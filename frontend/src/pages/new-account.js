35b0dd945485d9668cd168e55ce1d1038ff2bd8f
Martini310 <martin310@wp.pl>
2023-12-08 20:40:33 +0100

    Update page headers

--- frontend/src/pages/new-account.js
+++ frontend/src/pages/new-account.js
@@ -8,7 +8,7 @@ const Page = () => (
   <>
     <Head>
       <title>
-        Account | Devias Kit
+        Dodaj konto | Billwise
       </title>
     </Head>
     <Box
--- frontend/src/pages/settings.js
+++ frontend/src/pages/settings.js
@@ -8,7 +8,7 @@ const Page = () => (
   <>
     <Head>
       <title>
-        Settings | Devias Kit
+        Ustawienia | Billwise
       </title>
     </Head>
     <Box
