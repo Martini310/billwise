6db855aaa62f4c1c39edc49c1170bbddc9270855
Martini310 <martin310@wp.pl>
2023-12-10 21:15:51 +0100

    feat(api): Modify Account model list display in django admin panel

--- base/admin.py
+++ base/admin.py
@@ -4,5 +4,18 @@ from .models import Category, Supplier, Account, Invoice
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Supplier)
-admin.site.register(Account)
+# admin.site.register(Account)
 admin.site.register(Invoice)
+
+@admin.register(Account)
+class ProfileAdmin(admin.ModelAdmin):
+    """ Model admin for Profile. """
+    # List attributes.
+    list_display = (
+        '__str__',
+        'id',
+        'supplier',
+        'category',
+        'user',
+        'last_sync',
+    )
