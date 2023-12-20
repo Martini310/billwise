d4523e73b297d03cc2612ae6c29d5af5d2c7cc83
Martini310 <martin310@wp.pl>
2023-12-20 20:05:08 +0100

    Add koyeb to allowed hosts

--- billwise/settings.py
+++ billwise/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # DEBUG = 'IN_DOCKER' not in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'billwise-api-martini310.koyeb.app']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
