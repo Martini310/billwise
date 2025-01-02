3dfc3ff48ef6aee44ead02656e3bb0318c1a1c1f
Martin Brzeziński <martin310@wp.pl>
2025-01-02 22:17:37 +0100

    Commented creating superuser

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ python manage.py collectstatic --no-input
 # python manage.py makemigrations base
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser \
-  --no-input \
-  --username $DJANGO_SUPERUSER_USERNAME \
-  --email $DJANGO_SUPERUSER_EMAIL
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser \
+#   --no-input \
+#   --username $DJANGO_SUPERUSER_USERNAME \
+#   --email $DJANGO_SUPERUSER_EMAIL
+# fi
 
 # python manage.py runserver 0.0.0.0:8000
 # Start Gunicorn instead of runserver
